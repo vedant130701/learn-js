5fe854d7b4dfda978a4cfc9c6231eaced1bbf10e
Vedant Rishi Das <55937481+vedant130701@users.noreply.github.com>
2024-01-30 16:19:59 -0500

    information for arrow function in scripts/arrow.js

--- scripts/arrow.js
+++ scripts/arrow.js
@@ -1,10 +1,16 @@
+// this.firstName = "Jane" this matches with imp point
 const o = {
     msg: 'An object',
     person: {
         firstName: 'Grace',
         lastName: 'Hopper',
         age: 89,
-        fullName: () => { return this.firstName + ' ' + this.lastName }
+        // fullName: () => { return this.firstName + ' ' + this.lastName }
+        // using arrow operation, we get undefined undefined as scope not set
+        fullName: function() { return this.firstName + ' ' + this.lastName }
+        // using function operation, we get Grace Hopper as in scope
+
+        // imp point if we define this.firstName and this.lastName from before, define scope outside, then we can use arrow. i.e defined
     }
 }
 
