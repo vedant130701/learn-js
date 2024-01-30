3aff6729c6523d1aea6f67b05882455c980b5b12
Vedant Rishi Das <55937481+vedant130701@users.noreply.github.com>
2024-01-30 16:02:26 -0500

    Testing how to use functions in quiz/scripts/logger.js

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,19 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+const errorMsgsLogger = {
+    logMsg: function() {
+        if(this.errMsg) {
+            console.log(`error message: ${this.errMsg}`);
+        }
+    }
+}
+
+const error1 = {
+    errMsg : "This is an error 1"
+}
+
+const error2 = {
+    errMsg : "This is an error 2"
+}
+
+errorMsgsLogger.logMsg.call(error1);
+errorMsgsLogger.logMsg.call(error2);
