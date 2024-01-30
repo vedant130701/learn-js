658f4b5aa390a0af56ec0d23b294977a33bec7df
Vedant Rishi Das <55937481+vedant130701@users.noreply.github.com>
2024-01-30 16:37:29 -0500

    2nd type of closure information

--- quiz/scripts/closure.js
+++ quiz/scripts/closure.js
@@ -13,3 +13,21 @@ window.onload = function() {
     let toggle = true;
     document.getElementById('btn').onclick = setColor(toggle);
 }
+
+
+//or 
+
+// function setColor(set) {
+//     let changeColor = set;
+//     if (changeColor){
+//         let userColor = document.getElementById('color').value;
+//     document.getElementById('myPara').style.color = userColor;
+//     }
+//     // here we return a closure, so instead of a static function, a live one returns which uses closure based on changeColor
+// }
+
+// window.onload = function() {
+// let toggle = true;
+// document.getElementById('btn').onclick = () => setColor(toggle);
+// }
+// // this is also a closure
