7a38a5173b2b491aa7885604dad99e4170bb52f5
Vedant Rishi Das <55937481+vedant130701@users.noreply.github.com>
2024-02-06 15:37:02 -0500

    deep copy in js

--- quiz/scripts/map-deep.js
+++ quiz/scripts/map-deep.js
@@ -5,9 +5,17 @@ const originalMatrix = [
     [7, 9, 8]
   ];
   
+  // const newMatrix = originalMatrix.map((row) => {
+  //   return row.sort();
+  // }); // does shallow copy. Each row is a reference to the original matrix.
+
+  // const newMatrix = originalMatrix.map((row) => {
+  //   return Array.from(row).sort();
+  // }); // does shallow copy on each element. Since each element is primitive so does not matter.
+
   const newMatrix = originalMatrix.map((row) => {
-    return row.sort();
-  });
+    return [...row].sort();
+  }); // does shallow copy on each element. Since each element is primitive so does not matter. This is spread operator.
   
   // Modifying the original matrix (changing the last element of the first row)
   originalMatrix[0][2] = 99;
